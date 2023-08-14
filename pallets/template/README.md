404d9cb479978cb13e1e442260a7913d97d0c655
CocDap <hodinhdung1997@gmail.com>
2023-08-14 23:44:45 +0700

    feat: add code clecture 2 with add number transaction

--- pallets/template/README.md
+++ pallets/template/README.md
@@ -1,1 +1,21 @@
-License: Unlicense
+## Implement simple `add_number` extrinsic call
+
+### Build the project
+```
+cargo build
+```
+Noted: Should be in root directory
+### Run the project
+```
+./target/debug/node-template --dev
+```
+Noted: Should be in root directory
+
+### Interact with Polkadotjs Explorer
+
++ Go to `https://polkadot.js.org/apps/#/explorer` -> Choose `Development` -> `Local Node` -> `Switch`
+
++ Execute Transaction: Go to `Developer` -> `Extrinsics` -> Click `system` -> Switch to `templateModule` -> Choose `addNumber` extrinsics -> Enter your input
+
++ View on-chain storage: Go to `Developer` -> `Chain state` -> Click `timestamp` -> Switch to `templateModule` -> choose `something()` -> Click `Add Symbol` -> view on-chain storage 
+
--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -14,19 +14,22 @@ mod tests;
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
+
 #[frame_support::pallet]
 pub mod pallet {
+	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
-
+	
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
 	}
 
 	// The pallet's runtime storage items.
@@ -64,7 +67,7 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::call_index(0)]
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		#[pallet::weight(10_000)]
 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
@@ -82,7 +85,7 @@ pub mod pallet {
 
 		/// An example dispatchable that may throw a custom error.
 		#[pallet::call_index(1)]
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
+		#[pallet::weight(10_000)]
 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
 			let _who = ensure_signed(origin)?;
 
@@ -98,6 +101,28 @@ pub mod pallet {
 					Ok(())
 				},
 			}
+		}
+
+
+		#[pallet::call_index(2)]
+		#[pallet::weight(10_000)]
+		pub fn add_number(origin: OriginFor<T>, something: u32) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/main-docs/build/origins/
+			let who = ensure_signed(origin)?;
+
+			let current_value = Something::<T>::get().unwrap_or_default();
+
+			let new_value = current_value + something;
+
+			// Update storage.
+			<Something<T>>::put(new_value);
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored { something, who });
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
 		}
 	}
 }
